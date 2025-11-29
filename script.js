30e7aa50501db7a737cdc23ce1bcc3bf802f9d20
dicey44 <226594125+dicey44@users.noreply.github.com>
2025-11-29 14:12:01 -0500

    add JS variables

---
+++ script.js
@@ -0,0 +1,4 @@
+const addNewBtn = document.getElementById("add-new-btn");
+const statusDiv = document.getElementById("status-div");
+const statusMenuBtn = document.querySelectorAll(".status-menu-btn");
+const statusBtn = document.querySelectorAll(".status-btn");
