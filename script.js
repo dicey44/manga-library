17d4cff8fc6da4be62131342bb50f5c9bcb0f2ca
dicey44 <226594125+dicey44@users.noreply.github.com>
2025-12-01 11:09:27 -0500

    fix menu button not showing issue

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const addNewBtn = document.getElementById("add-new-btn");
 const statusDiv = document.getElementById("status-div");
 const statusMenuBtn = document.querySelectorAll(".status-menu-btn");
 const statusBtn = document.querySelectorAll(".status-btn");
+
+const openStatusMenu = () => {
+    statusDiv.style.display = "flex";
+} 
+
+statusBtn.forEach(btn => {
+    btn.addEventListener("click", openStatusMenu);
+});
