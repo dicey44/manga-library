08704fa03f7226c38213d13b18b3603fdc21143e
dicey44 <226594125+dicey44@users.noreply.github.com>
2025-12-02 14:46:07 -0500

    Create Edit Status Function
    The function takes the selected input and puts changes the button style and text, and updates the manga's object. If selected status is the same, it will just close the menu.

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function createManga(title, author, status) {
     const newManga = new Manga("Naruto", "Bob", "Finished");
 
     const row = document.createElement("tr");
-    row.id = newManga.id;
+    row.dataset.id = newManga.id;
     row.innerHTML = `
         <td>${entryNumber}</td>
         <td><img src="https://static.vecteezy.com/system/resources/previews/022/059/000/non_2x/no-image-available-icon-vector.jpg"/></td>
@@ -49,8 +49,63 @@ const openStatusMenu = () => {
     console.log(selectedMangaId);
 } 
 
+const closeStatusMenu = () => {
+    statusDiv.style.display = "none";
+}
+
 const editStatus = (selected) => {
-    //TO-DO
+    const mangaToChange = document.querySelector(`tr[data-id="${selectedMangaId}"]`);
+    const btnToChange = mangaToChange.querySelector(".status-btn");
+    const mangaObj = library.find(m => m.id === selectedMangaId);
+    if (selected === "planned") {
+        console.log("you selected planned");
+        if (btnToChange.classList.contains("planned")) {
+            selectedMangaId = null;
+            console.log(selectedMangaId);
+            closeStatusMenu();
+            return;
+        } else {
+            closeStatusMenu();
+            btnToChange.classList.remove("finished", "reading");
+            btnToChange.classList.add("planned");
+            btnToChange.textContent = "Planned";
+            mangaObj.status = "Planned";
+            console.log(mangaObj);
+            selectedMangaId = null;
+        }
+    } else if (selected === "reading") {
+        console.log("you selected reading");
+        if (btnToChange.classList.contains("reading")) {
+            selectedMangaId = null;
+            closeStatusMenu();
+            return;
+        } else {
+            closeStatusMenu();
+            btnToChange.classList.remove("finished", "planned");
+            btnToChange.classList.add("reading");
+            btnToChange.textContent = "Reading";
+            mangaObj.status = "Reading";
+            console.log(mangaObj);
+            selectedMangaId = null;
+        }
+    } else if (selected === "finished") {
+        console.log("you selected finished");
+        if (btnToChange.classList.contains("finished")) {
+            selectedMangaId = null;
+            closeStatusMenu();
+            return;
+        } else {
+            closeStatusMenu();
+            btnToChange.classList.remove("planned", "reading");
+            btnToChange.classList.add("finished");
+            btnToChange.textContent = "Finished";
+            mangaObj.status = "Finished";
+            console.log(mangaObj);
+            selectedMangaId = null;           
+        }
+    }
+
+
 }
 
 
@@ -60,12 +115,13 @@ list.addEventListener("click", (e) => {
     const row = e.target.closest("tr");
     if (!row) return;
 
-    const id = row.id;
+    const id = row.dataset.id;
     if (e.target.classList.contains("status-btn")) {
         selectedMangaId = id;
         openStatusMenu();
-}
-})
+    }
+    return;
+});
 
 addNewBtn.forEach(btn => {
     btn.addEventListener("click", createManga);
@@ -74,5 +130,9 @@ addNewBtn.forEach(btn => {
 statusDiv.addEventListener("click", (e) => {
     if (e.target.classList.contains("planned")) {
         editStatus("planned");
+    } else if (e.target.classList.contains("reading")) {
+        editStatus("reading");
+    } else if (e.target.classList.contains("finished")) {
+        editStatus("finished");
     }
-})
+});
